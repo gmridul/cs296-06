f5cefa3e22b7eb55adca06cba392f1174366f968
Sagar <sagar@ubuntu.ubuntu-domain>
2013-03-16 21:51:33 +0530

    Rerun the csv script

--- scripts/g06_gen_csv.py
+++ scripts/g06_gen_csv.py
@@ -7,8 +7,9 @@ f=open('../data/lab05_g06_data.csv','w+')
 for rerun in range(1,100):
     for iter in range(1,100):
         data=[]
-        out=subprocess.Popen(["../mybins/cs296_exe_06",str(iter)],stdout=subprocess.PIPE)
-        out=out.stdout.read().decode("utf-8")
+        #out=subprocess.Popen(["../mybins/cs296_exe_06",str(iter)],stdout=subprocess.PIPE)
+        out=subprocess.check_output(["../mybins/cs296_exe_06",str(iter)])
+        out=out.decode("utf-8")
 
         data=re.findall('[\x30-\x39\x2E]+',out)
         f.write(str(rerun))
