589d133f7b087079a257e734bb677434fb07a2e2
Sagar <sagar@ubuntu.ubuntu-domain>
2013-03-23 22:03:11 +0530

    Added the BookStatistics.java file

--- java/BookAnalysisG06.java
+++ java/BookAnalysisG06.java
@@ -1,4 +1,3 @@
-
 import cs296BookAnalysis.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,8 +10,11 @@ public class BookAnalysisG06 {
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Book mybook = new Book(args[0]);
-		System.out.println(mybook.getBookTitle());
-		System.out.println(mybook.getBookAuthor());
+	System.out.println(mybook.getBookTitle());
+	System.out.println(mybook.getBookAuthor());
+	BookStatistics bookStats = new BookStatistics(args[0]);
+	System.out.println(bookStats.countBookWords());
+	System.out.println(bookStats.findBookCharacters()); 
     }
 }
 
--- java/cs296BookAnalysis/Book.java
+++ java/cs296BookAnalysis/Book.java
@@ -2,75 +2,104 @@ package cs296BookAnalysis;
 
 import java.io.*;
 
-public class Book {
-    
-	String bookFilename ;
-	String author=null,title=null;
+public class Book 
+{
+    String bookFileName ;
+    String author=null,title=null;
+    String[] words = null;
 	
-	public Book(String path) throws FileNotFoundException {
-            bookFilename = path; 
- 	}
+    public Book(String path) throws FileNotFoundException 
+    {
+	bookFileName = path; 
+    }
 
- 	public String getBookTitle() throws IOException {
- 		
-            BufferedReader br ;
-            br = new BufferedReader(new FileReader(bookFilename));
-            if(title == null)
-                    while(true) {
-                            String line = br.readLine();
-                            if(line.contains("Title:")) {
-                                    title=line.substring(7);
-                                    br.close();
-                                    return title;
-                            }
-                    }
-            else
-                    return title;
- 	}
+    public String getBookTitle() throws IOException 
+    {		
+	BufferedReader br ;
+	br = new BufferedReader(new FileReader(bookFileName));
+	if(title == null)
+	    while(true) 
+		{
+		    String line = br.readLine();
+		    if(line.contains("Title:")) 
+			{
+			    br.close();
+			    title = line.substring(7);
+			    return title;
+			}
+		}
+	else
+	    return title;
+    }
 
-	public String getBookAuthor() throws IOException {
- 		BufferedReader br ;
-                br = new BufferedReader(new FileReader(bookFilename));
- 		if(author == null)
-	 		while(true) {
-	 			String line = br.readLine();
-	 			if(line.contains("Author:")) {
-	 				author=line.substring(8);
-                                        br.close();
-	 				return author;
-	 			}
-	 		}
-	 	else
-	 		return author;
- 	}
+    public String getBookAuthor() throws IOException 
+    {
+	BufferedReader br ;
+	br = new BufferedReader(new FileReader(bookFileName));
+	if(author == null)
+	    while(true) 
+		{
+		    String line = br.readLine();
+		    if(line.contains("Author:")) 
+			{
+			    author=line.substring(8);
+			    br.close();
+			    return author;
+			}
+		}
+	else
+	    return author;
+    }
 
- 	public void printBookTitle() throws IOException {
-            if(title==null) {
-                this.getBookTitle();
-            }
-            System.out.println(title);
- 	}
+    public void printBookTitle() throws IOException 
+    {
+	if(title==null) 
+	    {
+		this.getBookTitle();
+	    }
+	System.out.println(title);
+    }
 
- 	public void printBookAuthor() throws IOException {
-            if(author==null) {
-                this.getBookAuthor();
-            }
-            System.out.println(author);
- 	}
+    public void printBookAuthor() throws IOException 
+    {
+	if(author==null) 
+	    {
+		this.getBookAuthor();
+	    }
+	System.out.println(author);
+    }
 
- 	public String[] getBookWords() throws IOException {
-            BufferedReader br ;
-            br = new BufferedReader(new FileReader(bookFilename));
-            String temp = null;
-            String text = null;
-            while(true) {
-                temp = br.readLine();
-                if(temp == null)
-                    break;
-                text+=temp;
-            }
-            br.close();
-            return text.split("\\s++");
- 	}
- }
+    public String[] getBookWords() throws IOException 
+    {
+	if (words != null)
+	    return words;
+	BufferedReader br ;
+	br = new BufferedReader(new FileReader(bookFileName));
+	String temp = null;
+	String text = null;
+	while(true) {
+	    temp = br.readLine();	    
+	    if (temp == null)
+		break;
+	    temp = temp.replace (","," ");
+	    temp = temp.replace ("-"," ");
+	    temp = temp.replace ("\"","");
+	    temp = temp.replace (";"," ");
+	    temp = temp.replace (":"," ");
+	    text += temp + " ";
+	}
+	br.close();
+	words = text.split(" ");
+	return words;
+    }
+
+    public String[] getBookCharacters() throws IOException
+    {
+	BufferedReader br;
+	br = new BufferedReader(new FileReader(bookFileName));
+	if (words == null)
+	    words = getBookWords();
+	return null;
+    }
+}
 
