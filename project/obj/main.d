465f216c1f9595ad8e470d8aa824439b94dffd1e
Sudipto <sudipto@sudipto-Ideapad-Z570.(none)>
2013-04-09 21:13:05 +0530

    smooth slides

--- project/obj/main.d
+++ project/obj/main.d
@@ -1,56 +1,55 @@
-/home/mridul/cs296-06/project//obj/main.o: \
- /home/mridul/cs296-06/project//src/main.cpp \
- /home/mridul/cs296-06/project//src/render.hpp \
- /home/mridul/cs296-06/project//external/include/Box2D/Box2D.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Common/b2Settings.h \
+/home/sudipto/cs296-06/project//obj/main.o: \
+ /home/sudipto/cs296-06/project//src/main.cpp \
+ /home/sudipto/cs296-06/project//src/render.hpp \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Box2D.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Common/b2Settings.h \
  /usr/include/c++/4.6/cassert /usr/include/assert.h \
- /usr/include/features.h /usr/include/x86_64-linux-gnu/bits/predefs.h \
- /usr/include/x86_64-linux-gnu/sys/cdefs.h \
- /usr/include/x86_64-linux-gnu/bits/wordsize.h \
- /usr/include/x86_64-linux-gnu/gnu/stubs.h \
- /usr/include/x86_64-linux-gnu/gnu/stubs-64.h /usr/include/c++/4.6/cmath \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/c++config.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/os_defines.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/cpu_defines.h \
+ /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h /usr/include/c++/4.6/cmath \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/c++config.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/os_defines.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/cpu_defines.h \
  /usr/include/c++/4.6/bits/cpp_type_traits.h \
  /usr/include/c++/4.6/ext/type_traits.h /usr/include/math.h \
- /usr/include/x86_64-linux-gnu/bits/huge_val.h \
- /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
- /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
- /usr/include/x86_64-linux-gnu/bits/inf.h \
- /usr/include/x86_64-linux-gnu/bits/nan.h \
- /usr/include/x86_64-linux-gnu/bits/mathdef.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
- /usr/include/x86_64-linux-gnu/bits/mathinline.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Common/b2Draw.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Common/b2Math.h \
+ /usr/include/i386-linux-gnu/bits/huge_val.h \
+ /usr/include/i386-linux-gnu/bits/huge_valf.h \
+ /usr/include/i386-linux-gnu/bits/huge_vall.h \
+ /usr/include/i386-linux-gnu/bits/inf.h \
+ /usr/include/i386-linux-gnu/bits/nan.h \
+ /usr/include/i386-linux-gnu/bits/mathdef.h \
+ /usr/include/i386-linux-gnu/bits/mathcalls.h \
+ /usr/include/i386-linux-gnu/bits/mathinline.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Common/b2Draw.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Common/b2Math.h \
  /usr/include/c++/4.6/cfloat \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include/float.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/float.h \
  /usr/include/c++/4.6/cstddef \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stddef.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h \
  /usr/include/c++/4.6/limits \
- /home/mridul/cs296-06/project//external/include/Box2D/Common/b2Timer.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/Shapes/b2CircleShape.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/Shapes/b2Shape.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Common/b2BlockAllocator.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/b2Collision.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Common/b2Timer.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/Shapes/b2CircleShape.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/Shapes/b2Shape.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Common/b2BlockAllocator.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/b2Collision.h \
  /usr/include/c++/4.6/climits \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/limits.h \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/syslimits.h \
- /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
- /usr/include/x86_64-linux-gnu/bits/local_lim.h \
- /usr/include/linux/limits.h \
- /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
- /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
- /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/Shapes/b2EdgeShape.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/Shapes/b2ChainShape.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/Shapes/b2PolygonShape.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/b2BroadPhase.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/b2DynamicTree.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Common/b2GrowableStack.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include-fixed/limits.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/i386-linux-gnu/bits/posix1_lim.h \
+ /usr/include/i386-linux-gnu/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/i386-linux-gnu/bits/posix2_lim.h \
+ /usr/include/i386-linux-gnu/bits/xopen_lim.h \
+ /usr/include/i386-linux-gnu/bits/stdio_lim.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/Shapes/b2EdgeShape.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/Shapes/b2ChainShape.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/Shapes/b2PolygonShape.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/b2BroadPhase.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/b2DynamicTree.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Common/b2GrowableStack.h \
  /usr/include/c++/4.6/cstring /usr/include/string.h \
- /usr/include/xlocale.h /usr/include/x86_64-linux-gnu/bits/string3.h \
+ /usr/include/xlocale.h /usr/include/i386-linux-gnu/bits/string3.h \
  /usr/include/c++/4.6/algorithm /usr/include/c++/4.6/utility \
  /usr/include/c++/4.6/bits/stl_relops.h \
  /usr/include/c++/4.6/bits/stl_pair.h /usr/include/c++/4.6/bits/move.h \
@@ -64,74 +63,74 @@
  /usr/include/c++/4.6/bits/stl_iterator.h \
  /usr/include/c++/4.6/debug/debug.h /usr/include/c++/4.6/bits/stl_algo.h \
  /usr/include/c++/4.6/cstdlib /usr/include/stdlib.h \
- /usr/include/x86_64-linux-gnu/bits/waitflags.h \
- /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
- /usr/include/x86_64-linux-gnu/bits/endian.h \
- /usr/include/x86_64-linux-gnu/bits/byteswap.h \
- /usr/include/x86_64-linux-gnu/sys/types.h \
- /usr/include/x86_64-linux-gnu/bits/types.h \
- /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/time.h \
- /usr/include/x86_64-linux-gnu/sys/select.h \
- /usr/include/x86_64-linux-gnu/bits/select.h \
- /usr/include/x86_64-linux-gnu/bits/sigset.h \
- /usr/include/x86_64-linux-gnu/bits/time.h \
- /usr/include/x86_64-linux-gnu/bits/select2.h \
- /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
- /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
- /usr/include/x86_64-linux-gnu/bits/stdlib.h \
+ /usr/include/i386-linux-gnu/bits/waitflags.h \
+ /usr/include/i386-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/i386-linux-gnu/bits/endian.h \
+ /usr/include/i386-linux-gnu/bits/byteswap.h \
+ /usr/include/i386-linux-gnu/sys/types.h \
+ /usr/include/i386-linux-gnu/bits/types.h \
+ /usr/include/i386-linux-gnu/bits/typesizes.h /usr/include/time.h \
+ /usr/include/i386-linux-gnu/sys/select.h \
+ /usr/include/i386-linux-gnu/bits/select.h \
+ /usr/include/i386-linux-gnu/bits/sigset.h \
+ /usr/include/i386-linux-gnu/bits/time.h \
+ /usr/include/i386-linux-gnu/bits/select2.h \
+ /usr/include/i386-linux-gnu/sys/sysmacros.h \
+ /usr/include/i386-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/i386-linux-gnu/bits/stdlib.h \
  /usr/include/c++/4.6/bits/algorithmfwd.h \
  /usr/include/c++/4.6/initializer_list \
  /usr/include/c++/4.6/bits/stl_heap.h \
  /usr/include/c++/4.6/bits/stl_tempbuf.h \
  /usr/include/c++/4.6/bits/stl_construct.h /usr/include/c++/4.6/new \
  /usr/include/c++/4.6/exception \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/b2Distance.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Collision/b2TimeOfImpact.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/b2Body.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/b2Distance.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Collision/b2TimeOfImpact.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/b2Body.h \
  /usr/include/c++/4.6/memory /usr/include/c++/4.6/bits/allocator.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/c++allocator.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/c++allocator.h \
  /usr/include/c++/4.6/ext/new_allocator.h \
  /usr/include/c++/4.6/bits/stl_uninitialized.h \
  /usr/include/c++/4.6/bits/stl_raw_storage_iter.h \
  /usr/include/c++/4.6/backward/auto_ptr.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/b2Fixture.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/b2WorldCallbacks.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/b2TimeStep.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/b2World.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Common/b2StackAllocator.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/b2ContactManager.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Contacts/b2Contact.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2DistanceJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2Joint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2FrictionJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2GearJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2WheelJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2MouseJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2PrismaticJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2PulleyJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2RevoluteJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2RopeJoint.h \
- /home/mridul/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2WeldJoint.h \
- /home/mridul/cs296-06/project//src/cs296_base.hpp \
- /home/mridul/cs296-06/project//src/callbacks.hpp /usr/include/GL/glui.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/b2Fixture.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/b2WorldCallbacks.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/b2TimeStep.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/b2World.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Common/b2StackAllocator.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/b2ContactManager.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Contacts/b2Contact.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2DistanceJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2Joint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2FrictionJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2GearJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2WheelJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2MouseJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2PrismaticJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2PulleyJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2RevoluteJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2RopeJoint.h \
+ /home/sudipto/cs296-06/project//external/include/Box2D/Dynamics/Joints/b2WeldJoint.h \
+ /home/sudipto/cs296-06/project//src/cs296_base.hpp \
+ /home/sudipto/cs296-06/project//src/callbacks.hpp /usr/include/GL/glui.h \
  /usr/include/GL/glut.h /usr/include/GL/freeglut_std.h \
  /usr/include/GL/gl.h /usr/include/GL/glext.h /usr/include/inttypes.h \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stdint.h /usr/include/stdint.h \
- /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/GL/glu.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stdint.h /usr/include/stdint.h \
+ /usr/include/i386-linux-gnu/bits/wchar.h /usr/include/GL/glu.h \
  /usr/include/c++/4.6/cstdio /usr/include/stdio.h /usr/include/libio.h \
  /usr/include/_G_config.h /usr/include/wchar.h \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stdarg.h \
- /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
- /usr/include/x86_64-linux-gnu/bits/stdio.h \
- /usr/include/x86_64-linux-gnu/bits/stdio2.h /usr/include/c++/4.6/string \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stdarg.h \
+ /usr/include/i386-linux-gnu/bits/sys_errlist.h \
+ /usr/include/i386-linux-gnu/bits/stdio.h \
+ /usr/include/i386-linux-gnu/bits/stdio2.h /usr/include/c++/4.6/string \
  /usr/include/c++/4.6/bits/stringfwd.h \
  /usr/include/c++/4.6/bits/char_traits.h \
  /usr/include/c++/4.6/bits/postypes.h /usr/include/c++/4.6/cwchar \
- /usr/include/x86_64-linux-gnu/bits/wchar2.h \
+ /usr/include/i386-linux-gnu/bits/wchar2.h \
  /usr/include/c++/4.6/bits/localefwd.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/c++locale.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/c++locale.h \
  /usr/include/c++/4.6/clocale /usr/include/locale.h \
- /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/4.6/iosfwd \
+ /usr/include/i386-linux-gnu/bits/locale.h /usr/include/c++/4.6/iosfwd \
  /usr/include/c++/4.6/cctype /usr/include/ctype.h \
  /usr/include/c++/4.6/bits/ostream_insert.h \
  /usr/include/c++/4.6/bits/cxxabi_forced.h \
@@ -140,17 +139,17 @@
  /usr/include/c++/4.6/bits/range_access.h \
  /usr/include/c++/4.6/bits/basic_string.h \
  /usr/include/c++/4.6/ext/atomicity.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/gthr.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/gthr-default.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/gthr.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/gthr-default.h \
  /usr/include/pthread.h /usr/include/sched.h \
- /usr/include/x86_64-linux-gnu/bits/sched.h \
- /usr/include/x86_64-linux-gnu/bits/timex.h \
- /usr/include/x86_64-linux-gnu/bits/setjmp.h /usr/include/unistd.h \
- /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
- /usr/include/x86_64-linux-gnu/bits/environments.h \
- /usr/include/x86_64-linux-gnu/bits/confname.h /usr/include/getopt.h \
- /usr/include/x86_64-linux-gnu/bits/unistd.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/atomic_word.h \
+ /usr/include/i386-linux-gnu/bits/sched.h \
+ /usr/include/i386-linux-gnu/bits/timex.h \
+ /usr/include/i386-linux-gnu/bits/setjmp.h /usr/include/unistd.h \
+ /usr/include/i386-linux-gnu/bits/posix_opt.h \
+ /usr/include/i386-linux-gnu/bits/environments.h \
+ /usr/include/i386-linux-gnu/bits/confname.h /usr/include/getopt.h \
+ /usr/include/i386-linux-gnu/bits/unistd.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/atomic_word.h \
  /usr/include/c++/4.6/bits/basic_string.tcc /usr/include/c++/4.6/vector \
  /usr/include/c++/4.6/bits/stl_vector.h \
  /usr/include/c++/4.6/bits/stl_bvector.h \
